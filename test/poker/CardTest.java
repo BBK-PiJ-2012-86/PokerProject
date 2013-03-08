4fe991596529e9e1fbfa79712da694c35825096a
BBK-PiJ-2012-88 <edhogarth@hotmail.com>
2013-03-08 15:17:05 +0000

    Card passing all tests

--- test/poker/CardTest.java
+++ test/poker/CardTest.java
@@ -29,5 +29,40 @@ public class CardTest {
 		String result = card.toString();
 		assertEquals(expected, result);
 	}
+	
+	@Test
+	public void testRanktoRankConversionInt(){
+		Rank rank = Rank.Eight;
+		Suit suit = Suit.Clubs;
+				
+		Card card = new Card(rank, suit);
+		int expected = 8;
+		int result = card.getRankInt();
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testRanktoRankConversionRank(){
+		Card card = new Card(4,3);
+		Rank expected = Rank.Four;
+		Rank result = card.getRank();
+		assertEquals(expected, result);
+		
+	}
+	
+	@Test
+	public void testSuitToSuitConversionInt(){
+		Rank rank = Rank.Jack;
+		Suit suit = Suit.Spades;
+		Card card = new Card(rank, suit);
+		assertEquals(1, card.getSuitInt());
+	}
+	
+	@Test
+	public void testSuitToSuitConversionSuit(){
+		Card card = new Card(14,2);
+		Suit suit = Suit.Clubs;
+		assertEquals(suit, card.getSuit());
+	}
 
 }
