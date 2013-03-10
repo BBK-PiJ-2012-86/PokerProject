3c63b3db204ef89a90091ffd65a521acdf8dd05a
BBK-PiJ-2012-86 <ruthroygbiv@yahoo-co-uk>
2013-03-10 08:48:13 +0000

    Outline for CheckerImplTest

---
+++ test/poker/CheckerImplTest.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package poker;
+
+import static org.junit.Assert.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author 86
+ *
+ */
+public class CheckerImplTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	@Test
+	public void testCheck() {
+		
+	}
+
+}
