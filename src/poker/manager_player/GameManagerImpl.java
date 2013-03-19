403aff7f4467b27c695319f769442a71818eef6b
BBK-PiJ-2012-88 <edhogarth@hotmail.com>
2013-03-19 11:01:34 +0000

    hopefully resolved conflict

---
+++ src/poker/manager_player/GameManagerImpl.java
@@ -0,0 +1,115 @@
+package poker.manager_player;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import poker.hand_card.Card;
+import poker.hand_card.Deck;
+import poker.hand_card.DeckFactory;
+
+
+
+/**
+ * This is the manager class for the poker game
+ *
+ */
+public class GameManagerImpl implements GameManager {
+
+	private CircularArrayList<Player> players = new CircularArrayList<>();
+	private Deck deck = null;
+	private GameType gameType;
+
+	public GameManagerImpl(GameType gameType/*,CircularArrayList<Player> players*/) {
+		this.gameType = gameType;
+		/*this.deck = null;
+		this.players = players;
+		for(Player player: players){
+			player.changeGameType(type);
+		}*/
+	}
+
+	/* (non-Javadoc)
+	 * @see poker.manager_player.GameManager#addPlayer(poker.manager_player.Player)
+	 */
+	@Override
+	public void addPlayer(Player player){
+		player.changeGameType(gameType);
+		players.add(player);
+	}
+
+	/* (non-Javadoc)
+	 * @see poker.manager_player.GameManager#deal()
+	 */
+	@Override
+	public void deal(){
+		int numCards = gameType.numCards();
+		deck = DeckFactory.getDeckFactory().getDeck();
+		for(Player player: players){
+			player.recieveCards(deck.dealCards(numCards));
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see poker.manager_player.GameManager#playRound()
+	 */
+	@Override
+	public void playRound(){
+		deal();
+		playersChangeCards();
+		evaluateWinner();
+		deletePlayerCards();
+	}
+
+	/* (non-Javadoc)
+	 * @see poker.manager_player.GameManager#playersChangeCards()
+	 */
+	@Override
+	public void playersChangeCards(){
+		for(Player player: players){
+			int cardsToSwap = player.exchangeCards();
+			if(cardsToSwap > 0){
+				List<Card> cards = new ArrayList<Card>();
+				for(int i = 0; i < cardsToSwap; i++){
+					cards.add(deck.getCards().get(0));
+					deck.getCards().remove(0);
+				}
+				player.recieveCards(cards);
+			}
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see poker.manager_player.GameManager#evaluateWinner()
+	 */
+	@Override
+	public void evaluateWinner(){
+		List<Player> tempList = new ArrayList<Player>();
+		for(Player player: players){
+			tempList.add(player);
+		}
+		Comparator<Player> c = players.getTheDealer().getCheckResultRanking();
+		Collections.sort(tempList, c);
+		System.out.println("The winner is " + tempList.get(0));
+		System.out.println("Their hand was " + tempList.get(0).getHand().toString());
+	}
+
+	/* (non-Javadoc)
+	 * @see poker.manager_player.GameManager#deletePlayerCards()
+	 */
+	@Override
+	public void deletePlayerCards(){
+		for(Player player: players){
+			player.removeCards();
+		}
+	}
+
+	public static void main(String [] args){
+		CircularArrayList<Player> list = new CircularArrayList<Player>();
+		GameManager game = new GameManagerImpl(GameType.FIVE_CARD_DRAW);
+		game.launch();
+	}
+
+}
