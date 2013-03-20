2603dc5c11a7de2369a7af4ea78aa0ca699dcf08
BBK-PiJ-2012-86 <ruthroygbiv@yahoo-co-uk>
2013-03-20 12:37:08 +0000

    Input validation working well - code needs refactoring

--- src/poker/manager_player/HumanPlayerConsoleInterface.java
+++ src/poker/manager_player/HumanPlayerConsoleInterface.java
@@ -10,7 +10,7 @@ import poker.hand_card.Hand;
  * A HumanPayer plays poker and needs input to make its card replacement decisions
  *
  */
-public class HumanPlayerConsoleInterface implements HumanPlayerListener {
+public class HumanPlayerConsoleInterface implements HumanPlayerListener {	//TODO: needs refactoring
   /* Problem (with line above):    ?????
   Dm: Reliance on default encoding (DM_DEFAULT_ENCODING)
   Found a call to a method which will perform a byte to String (or String to byte) conversion, and will assume
@@ -30,39 +30,58 @@ public class HumanPlayerConsoleInterface implements HumanPlayerListener {
   public int getCountOfCardsToSwap(int max) {
     System.out.println("How many cards would you like to swap (max " + max + ")?");
     int swap = 0;
+    
+    boolean needValid = true;
     boolean needInt = true;
-    while (needInt) {
-      String s = scanner.nextLine();
-      try{
-          swap = Integer.parseInt(s);
-          needInt = false;
-      }
-      catch(NumberFormatException ex){
-          System.out.println("Please enter a single numerical digit e.g. 1");
-      }
-    }
-          
-    while(swap > max || swap < 0){
-      System.out.println("That is not a valid selection, you cannot swap more than " + max + " cards");
-      System.out.println("How many cards would you like to swap (max " + max + ")?");
-      swap = scanner.nextInt();
+    
+    while (needValid) {
+	    while (needInt) {
+	      String s = scanner.nextLine();
+	      try{
+	          swap = Integer.parseInt(s);
+	          needInt = false;
+	      }
+	      catch(NumberFormatException ex){
+	          System.out.println("Please enter a single numerical digit e.g. 1");
+	      }
+	    }
+	    if (swap>max|| swap<0) {
+	    	System.out.println("That is not a valid selection, you cannot swap more than " + max + " cards");
+	        System.out.println("How many cards would you like to swap (max " + max + ")?");
+	        needInt = true;
+	    } else {
+	    	needValid = false;
+	    }
     }
     return swap;
   }
   
   public Card selectCardsToRemove() {
-    System.out.println("Which card would you like to swap (From 1  5)?");
-    try{
-      int swap = scanner.nextInt();
-      while(swap > hand.getCards().size() || swap < 1){
-        System.out.println("That is not a valid selection, it was not between 1 and 5");
-        System.out.println("Which card would you like to swap (From 1  5)?");
-        swap = scanner.nextInt();
-      }
-      return hand.getCardAt(swap);
-    } catch (NumberFormatException e){
-      selectCardsToRemove();
+    System.out.println("Which card would you like to swap (From 1 - 5)?");	// 5 to be hand size?
+    int swap = 0;
+
+    boolean needValid = true;
+    boolean needInt = true;
+    
+    while (needValid) {
+	    while (needInt) {
+	      String s = scanner.nextLine();
+	      try{
+	          swap = Integer.parseInt(s);
+	          needInt = false;
+	      }
+	      catch(NumberFormatException ex){
+	          System.out.println("Please enter a single numerical digit e.g. 1");
+	      }
+	    }
+	    if (swap>5|| swap<1) {
+	    	System.out.println("That is not a valid selection, it was not between 1 and 5");
+	        System.out.println("Which card would you like to swap (From 1 - 5)?");
+	        needInt = true;
+	    } else {
+	    	needValid = false;
+	    }
     }
-    return null;
+    return hand.getCardAt(swap);
   }
 }
