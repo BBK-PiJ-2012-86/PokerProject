fae287e4a8f51a095d032375ad650c4222a01a6f
BBK-PiJ-2012-86 <ruthroygbiv@yahoo-co-uk>
2013-03-21 20:01:41 +0000

    All HumanPlayerConsoleInterface tests passing!

--- test/poker/manager_player/HumanPlayerConsoleInterfaceTest.java
+++ test/poker/manager_player/HumanPlayerConsoleInterfaceTest.java
@@ -1,9 +1,12 @@
 package poker.manager_player;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static poker.hand_card.TestCards.*;
+import static poker.hand_card.TestCards.ACE_SPADE;
+import static poker.hand_card.TestCards.JACK_SPADE;
+import static poker.hand_card.TestCards.KING_SPADE;
+import static poker.hand_card.TestCards.QUEEN_SPADE;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -24,9 +27,20 @@ import poker.hand_card.TestUtil;
 public class HumanPlayerConsoleInterfaceTest {
 	
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-	String eo1 = System.getProperty("line.separator");
+	String eol = System.getProperty("line.separator");
 	private HumanPlayerConsoleInterface hpci;
-
+	private final static Hand mockHand = mock(Hand.class);
+	
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		Card[] cardArray = new Card[]{ACE_SPADE,KING_SPADE, QUEEN_SPADE, JACK_SPADE};
+		List<Card> cards = TestUtil.toLinkedList(cardArray);
+		when(mockHand.getCards()).thenReturn(cards);
+		when(mockHand.toString()).thenReturn("Hand");
+		when(mockHand.getCardAt(0)).thenReturn(ACE_SPADE);
+		when(mockHand.getCardAt(1)).thenReturn(KING_SPADE);
+	}
+	
 	@Before
 	public void setUp() throws Exception {
 		System.setOut(new PrintStream(outContent));
@@ -40,10 +54,8 @@ public class HumanPlayerConsoleInterfaceTest {
 
 	@Test
 	public void testOnReceiveCards() {
-		Hand mockHand = mock(Hand.class);
-		when(mockHand.toString()).thenReturn("Hand");
 		hpci.onReceiveCards(mockHand);
-		String expected = "Your hand is..."+eo1+"Hand"+eo1;
+		String expected = "Your hand is..."+eol+"Hand"+eol;
 		String actual = outContent.toString();
 		assertEquals(expected, actual);
 	}
@@ -51,7 +63,6 @@ public class HumanPlayerConsoleInterfaceTest {
 	@Test
 	public void testGetCountOfCardsToSwapZero() {
 		ByteArrayInputStream in = new ByteArrayInputStream("0".getBytes());
-		//System.setIn(in);
 		hpci.setScanner(new Scanner(in));
 		
 		int expectedInt = 0;
@@ -66,7 +77,6 @@ public class HumanPlayerConsoleInterfaceTest {
 	@Test
 	public void testGetCountOfCardsToSwapOne() {
 		ByteArrayInputStream in = new ByteArrayInputStream("1".getBytes());
-		//System.setIn(in);
 		hpci.setScanner(new Scanner(in));
 		
 		int expectedInt = 1;
@@ -80,9 +90,8 @@ public class HumanPlayerConsoleInterfaceTest {
 	
 	@Test
 	public void testGetCountOfCardsToSwapWrongNumber() {
-		String input = "9"+eo1+"0";
+		String input = "9"+eol+"0";
 		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
-		//System.setIn(in);
 		hpci.setScanner(new Scanner(in));
 		
 		int expectedInt = 0;
@@ -90,7 +99,7 @@ public class HumanPlayerConsoleInterfaceTest {
 		assertEquals(expectedInt, actualInt);
 		
 		String expectedStr = "How many cards would you like to swap (max 3): "+
-				"That is not a valid selection. Possible range 0 to 3"+eo1+
+				"That is not a valid selection. Possible range 0 to 3"+eol+
 				"Try again: ";
 		String actualStr = outContent.toString();
 		assertEquals(expectedStr, actualStr);
@@ -98,9 +107,8 @@ public class HumanPlayerConsoleInterfaceTest {
 
 	@Test
 	public void testGetCountOfCardsToSwapString() {
-		String input = "foo"+eo1+"0";
+		String input = "foo"+eol+"0";
 		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
-		//System.setIn(in);
 		hpci.setScanner(new Scanner(in));
 		
 		int expectedInt = 0;
@@ -108,7 +116,7 @@ public class HumanPlayerConsoleInterfaceTest {
 		assertEquals(expectedInt, actualInt);
 		
 		String expectedStr = "How many cards would you like to swap (max 3): "+
-				"Please enter a single numerical digit e.g. 1"+eo1+
+				"Please enter a single numerical digit e.g. 1"+eol+
 				"Try again: ";
 		String actualStr = outContent.toString();
 		assertEquals(expectedStr, actualStr);
@@ -127,15 +135,9 @@ public class HumanPlayerConsoleInterfaceTest {
 	
 	@Test
 	public void testSelectCardsToRemoveOne() {
-		Hand mockHand = mock(Hand.class);
-		Card[] cardArray = new Card[]{ACE_SPADE,KING_SPADE, QUEEN_SPADE, JACK_SPADE};
-		List<Card> cards = TestUtil.toLinkedList(cardArray);
-		when(mockHand.getCards()).thenReturn(cards);
-		when(mockHand.toString()).thenReturn("Hand");
-		when(mockHand.getCardAt(0)).thenReturn(ACE_SPADE);
 		hpci.onReceiveCards(mockHand);
 		
-		String input = "1"+eo1+"1";
+		String input = "1";
 		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
 		hpci.setScanner(new Scanner(in));
 		
@@ -144,24 +146,18 @@ public class HumanPlayerConsoleInterfaceTest {
 		List<Card> actualCards = hpci.selectCardsToRemove(1);
 		assertEquals(expectedCards, actualCards);
 		
-		String expectedStr = "Your hand is..."+eo1
-				+"Hand" +eo1
+		String expectedStr = "Your hand is..."+eol
+				+"Hand" +eol
 				+"Which card would you like to swap (From 1 - 4)?: ";
 		String actualStr = outContent.toString();
 		assertEquals(expectedStr, actualStr);
 	}
 	
 	@Test
-	public void testSelectCardsToRemoveRepeated() {
-		Hand mockHand = mock(Hand.class);
-		Card[] cardArray = new Card[]{ACE_SPADE,KING_SPADE, QUEEN_SPADE, JACK_SPADE};
-		List<Card> cards = TestUtil.toLinkedList(cardArray);
-		when(mockHand.getCards()).thenReturn(cards);
-		when(mockHand.toString()).thenReturn("Hand");
-		when(mockHand.getCardAt(0)).thenReturn(ACE_SPADE);
+	public void testSelectCardsToRemoveTwo() {
 		hpci.onReceiveCards(mockHand);
 		
-		String input = "1"+eo1+"1"+eo1+"2";
+		String input = "1"+eol+"2";
 		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
 		hpci.setScanner(new Scanner(in));
 		
@@ -169,14 +165,38 @@ public class HumanPlayerConsoleInterfaceTest {
 		expectedCards.add(ACE_SPADE);
 		expectedCards.add(KING_SPADE);
 		List<Card> actualCards = hpci.selectCardsToRemove(2);
-		assertEquals(expectedCards, actualCards);		// not working 
-									// not picking up second card..
+		assertEquals(expectedCards, actualCards);
 		
-		String expectedStr = "Your hand is..."+eo1
-				+"Hand" +eo1
+		String expectedStr = "Your hand is..."+eol
+				+"Hand" +eol
+				+"Which card would you like to swap (From 1 - 4)?: "
 				+"Which card would you like to swap (From 1 - 4)?: ";
 		String actualStr = outContent.toString();
 		assertEquals(expectedStr, actualStr);
 	}
 
+	@Test
+	public void testSelectCardsToRemoveRepeated() {
+		hpci.onReceiveCards(mockHand);
+		
+		String input = "1"+eol+"1"+eol+"2";
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+		hpci.setScanner(new Scanner(in));
+		
+		List<Card> expectedCards = new LinkedList<Card>();
+		expectedCards.add(ACE_SPADE);
+		expectedCards.add(KING_SPADE);
+		List<Card> actualCards = hpci.selectCardsToRemove(2);
+		assertEquals(expectedCards, actualCards);
+		
+		String expectedStr = "Your hand is..."+eol
+				+"Hand" +eol
+				+"Which card would you like to swap (From 1 - 4)?: "
+				+"Which card would you like to swap (From 1 - 4)?: "
+				+"You already picked that one. Try again."+eol
+				+"Which card would you like to swap (From 1 - 4)?: ";
+		String actualStr = outContent.toString();
+		assertEquals(expectedStr, actualStr);
+	}
+	
 }
