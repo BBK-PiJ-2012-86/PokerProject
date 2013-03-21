f37465428e58750c7ebb5b575f9578b0828bbaa6
BBK-PiJ-2012-86 <ruthroygbiv@yahoo-co-uk>
2013-03-21 21:57:19 +0000

    Attempting GameManagerImpl tests...

--- test/poker/manager_player/GameManagerImplTest.java
+++ test/poker/manager_player/GameManagerImplTest.java
@@ -1,11 +1,14 @@
 package poker.manager_player;
 
+import java.io.ByteArrayInputStream;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class GameManagerImplTest {
 
 	private GameManagerImpl gameManager;
+	private String eol = System.getProperty("line.separator");
 	
 	@Before
 	public void setUp(){
@@ -14,9 +17,22 @@ public class GameManagerImplTest {
 	}
 	
 	@Test
-	public void test() {
+	public void testChangeZero() {
+		String input = "0"+eol;
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
 		gameManager.addComputerPlayer(AiType.NORMAL);
 		gameManager.addHumanPlayer("Bob");
-		//gameManager.playRound();
+		gameManager.playRound();	//need to get inside this..
+	}
+	
+	@Test
+	public void testChangeOne() {
+		String input = "1"+eol+"1";
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+		gameManager.addComputerPlayer(AiType.NORMAL);
+		gameManager.addHumanPlayer("Bob");
+		gameManager.playRound();
 	}
 }
